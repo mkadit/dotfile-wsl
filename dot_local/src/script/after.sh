5d0b85d7f88cfa73477d9f9f168c15cc03ab087f
Muhammad Krishertanto Adityaputro <mkaditya00@gmail.com>
2025-06-28 15:25:07 +0700

    feat: change to use uv and bun

--- dot_local/src/script/after.sh
+++ dot_local/src/script/after.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 # go packages
-go install github.com/Zxilly/go-size-analyzer/cmd/gsa@latest Γöé
-go install github.com/vektra/mockery/v3@v3.3.2 Γöé
-go install github.com/xo/usql@latest Γöé
-go install -tags most github.com/xo/usql@master Γöé
-go install github.com/derailed/k9s@latest Γöé
-go install github.com/jesseduffield/lazydocker@latest Γöé
+go install github.com/Zxilly/go-size-analyzer/cmd/gsa@latest
+go install github.com/vektra/mockery/v3@v3.3.2
+go install github.com/xo/usql@latest
+go install -tags most github.com/xo/usql@master
+go install github.com/derailed/k9s@latest
+go install github.com/jesseduffield/lazydocker@latest
 
-# Install tailwindcss
+# tailwindcss
 curl -sLO https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64
 chmod +x tailwindcss-linux-x64
 mv tailwindcss-linux-x64 ~/.local/bin/tailwindcss
 
-# install git-filter-repo
-pip install --user pipx
-pipx install git-filter-repo
+# python tools
+uv tool install git-filter-repo
+
+# install nodejs tools
+bun install -g tailwindcss @tailwindcss/cli
