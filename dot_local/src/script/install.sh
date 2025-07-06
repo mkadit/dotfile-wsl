9a47b32863ac076e16ffcb6ef84233f3b1971330
Muhammad Krishertanto Adityaputro <mkaditya00@gmail.com>
2025-07-06 11:07:26 +0700

    feat: update install script

--- dot_local/src/script/install.sh
+++ dot_local/src/script/install.sh
@@ -40,17 +40,25 @@ curl https://mise.run | sh
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-cargo install ripgrep
-cargo install bob-nvim
-cargo install fd
+cargo install --git https://github.com/MordechaiHadad/bob --locked
+cargo install atuin
 cargo install btop
-cargo install tree-sitter
-cargo install navi
-cargo install zoxide
-cargo install mprocs
+cargo install fd-find
 cargo install fselect
+cargo install mprocs
+cargo install navi
+cargo install ripgrep
+cargo install tree-sitter-cli
+cargo install zoxide
 
 # Change shell to ZSH
 chsh -s "$(which zsh)"
 
 # install other binary
+
+# k3s
+curl -sfL https://get.k3s.io | sh -
+# k3d
+curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
