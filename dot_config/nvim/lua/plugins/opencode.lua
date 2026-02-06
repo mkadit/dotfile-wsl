4b60d144246e9b469f7af4f4afef239b83512ede
Muhammad Krishertanto Adityaputro <mkaditya00@gmail.com>
2026-02-06 17:18:12 +0700

    feat: update mise

--- dot_config/nvim/lua/plugins/opencode.lua
+++ dot_config/nvim/lua/plugins/opencode.lua
@@ -10,9 +10,9 @@ return {
   },
   config = function()
     ---@type opencode.Opts
-    vim.g.opencode_opts = {
-      -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-    }
+    -- vim.g.opencode_opts = {
+    --   -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+    -- }
 
     -- Required for `opts.events.reload`.
     vim.o.autoread = true
